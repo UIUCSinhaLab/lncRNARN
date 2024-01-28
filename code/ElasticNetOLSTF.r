38927c02695fcb7ed5d98cc69aef236c86ccc31c
XiaomanXie <43801931+XiaomanXie@users.noreply.github.com>
2024-01-28 15:59:41 -0800

    Add files via upload

--- code/ElasticNetOLSTF.r
+++ code/ElasticNetOLSTF.r
@@ -1,29 +1,27 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 
+library(data.table)
 library(glmnet)
 library(nortest)
 library(matrixStats)
 
-#data <- read.table("CPMMatrixForDNAMethylationFiltered_id.txt")
-data <- read.table("./result-confounder/r1/res.txt",header=F)
+data <- as.data.frame(fread("../data/CPMMatrix.noCov.txt",header=F))
 rownames(data) <- data[,1]
 data <- data[,-1]
 data <- as.matrix(data)
 data <- (data - rowSums(data))/rowSds(data)
 data <- as.data.frame(data)
 
-#lncrna_id <- read.table("lncRNA.txt",header=FALSE)[,1]
-#lncrna_id <- read.table("lncRNA_id.txt",header=FALSE)[,1]
-lncrna_id <- read.table("HumanTFList.txt",header=FALSE)[,1]
-pcrna_id <- read.table("ProteinCodingGeneID.txt",header=FALSE)[,1]
+lncrna_id <- read.table("../data/TF.txt",header=FALSE)[,1]
+pcrna_id <- read.table("../data/ProteinCodingGene.txt",header=FALSE)[,1]
 
 lncrna <- t(data[(rownames(data) %in% lncrna_id),])
 
 mrna <- data[(rownames(data) %in% pcrna_id),]
 
 lines <- c()
-for ( i in 1:13963 ){
+for ( i in 1:dim(mrna)[[1]] ){
 	p <- lillie.test(as.numeric(mrna[i,]))$p
 	if ( p > 0.0000000001 ){
 		lines <- c(lines,i)
@@ -31,18 +29,12 @@ for ( i in 1:13963 ){
 }
 mrna <- mrna[lines,]
 
-#lncrna <- as.matrix(cbind(lncrna,clinic))
-#colnames(lncrna) <- c(colnames(lncrna)[1:1255],"age","gender","american indian or alaska native","asian","black or african american","white")
-#mrna <- t(cbind(t(mrna),clinic))
-
 
 result <- c("id","cor_1","cor_2","cor_3","cor_4","cor_5","cor_ave","cor_all")
 ols <- c("id","R2")
-for ( i in args[1]:args[2]){
+for ( i in 1:dim(mrna)[[1]]){
 	re <- 0
 	y <- t(mrna[i,])
-
-	
 	rows <- which(!is.na(y[,1]))
 	pred <- cbind(colnames(y),length(rows)-floor(0.8*length(rows)))
 	row <- c(colnames(y))
@@ -66,18 +58,15 @@ for ( i in args[1]:args[2]){
 		row <- cbind(row,cc)
 		re <- re + cc
 	}
-	#write(pred,args[4],ncolumns=2+5*length(test_rows),append=TRUE)
 	print("all")
 	print(re)
 	row <- cbind(row,re/5)
 
-	#all.l.sequence <- glmnet(lncrna[,!(colnames(lncrna) %in% colnames(y))],y,alpha=0.5,family="gaussian",dfmax=5)$lambda
-	#print(all.l.sequence)
 	all.fit <- cv.glmnet(lncrna[,!(colnames(lncrna) %in% colnames(y))],y,dfmax=10,type.measure="mse",alpha=0.5,family="gaussian")
 	all.pred <- predict(all.fit,s=all.fit$lambda.min,newx=lncrna[,!(colnames(lncrna) %in% colnames(y))])
 	all.cc <- cor(y,all.pred)
 	row <- cbind(row,all.cc)
-	write.table(rbind(row),args[3],quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t",append=T)
+	write.table(rbind(row),"CVPerformanceTF.txt",quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t",append=T)
 	#result <- rbind(result,row)
 	
 	c<-coef(all.fit,s='lambda.min',exact=TRUE)
@@ -93,12 +82,8 @@ for ( i in args[1]:args[2]){
 	tss <- sum((y-mean(y))^2)
 
 	rowols <- c(colnames(y),r2,all.fit$lambda.min,(1-rss/tss))
-	#rowols <- cbind(rowols,r2)
-	write.table(rbind(rowols),args[6],quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t",append=T)
-	#ols <- rbind(ols,rowols)
-
 	rowres <- c(colnames(y),residuals(fit))
-	write.table(rbind(rowres),args[7],quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t",append=T)
+	write.table(rbind(rowres),"CPMMatrix.noCov.noTF.txt",quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t",append=T)
 
 	#c <- c[inds]
 	#var <- c(colnames(y))
@@ -111,11 +96,8 @@ for ( i in args[1]:args[2]){
 	var <- gsub("lncrna_ols","",var)
 	for (n in 1:length(var)){
                 tmp <- c(colnames(y),var[n],as.character(p[n]))
-                write.table(rbind(tmp),args[5],quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t",append=T)
+                write.table(rbind(tmp),"TFRegulators.txt",quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t",append=T)
         }	
 
-	#write(var,args[5],ncolumns=length(var),append=TRUE)
 }
 
-#write.table(result,args[3],quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t")
-#write.table(ols,args[6],quote=FALSE,row.names=FALSE,col.names=FALSE,sep="\t")
